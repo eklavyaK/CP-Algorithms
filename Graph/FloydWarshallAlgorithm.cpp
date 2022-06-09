0fa97e7f8df08838033da45cefcb19949508dce2
eklavya <eklavyakumarsingh100@gmail.com>
2022-06-09 23:50:05 +0530

    commit

--- Graph/FloydWarshallAlgorithm.cpp
+++ Graph/FloydWarshallAlgorithm.cpp
@@ -6,6 +6,9 @@ const int inf = 1e9+5;
 int main(){                       //Implementation of floyd warshall algorithm
     int n,m; cin>>n>>m;
     vector<vector<int>> dist(n+1,vector<int>(n+1,inf)); //distance matrix
+    for(int i=0;i<=n;i++){
+        dist[i][i]=0;
+    }
     for(int i=0;i<m;i++){
         int u,v,w;
         cin>>u>>v>>w;
