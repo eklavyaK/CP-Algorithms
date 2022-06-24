d9cd5224e6dec7b0ab78e1055ac0366375c6cb1e
eklavya <eklavyakumarsingh100@gmail.com>
2022-06-24 22:19:26 +0530

    commit

--- Graph/PrimsAlgorithm.cpp
+++ Graph/PrimsAlgorithm.cpp
@@ -49,3 +49,45 @@ int main(){
     cout<<primsMST(0, edge)<<endl;
     return 0;
 }
+/*******************************************SIMPLIFIED VERSION*************************************/
+// const int N = 1e5+5, inf = 1e9+5;
+// vector<vector<pair<int,int>>> node(N);
+
+// int main(){
+//     rapid_iostream;
+//     int n,m; cin>>n>>m;
+//     while(m--){
+//         int u,v,c; cin>>u>>v>>c;
+//         node[u].push_back({v,c});
+//         node[v].push_back({u,c});
+//     }
+//     vector<int> cost(n+1,inf);
+//     ll ans = 0; 
+//     cost[1]=0;
+//     bool check[n+1]{};
+//     set<pair<int,int>> st;
+//     st.insert({0,1});
+//     while(!st.empty()){
+//         auto [c,u] = *st.begin();
+//         ans += c; check[u]=true;
+//         for(auto [nod,cos] : node[u]){
+//             if(!check[nod]){
+//                 if(cost[nod]>cos){
+//                     st.erase({cost[nod],nod});
+//                     cost[nod] = cos;
+//                     st.insert({cost[nod],nod});
+//                 }
+//             }
+//         }
+//         st.erase({c,u});
+//     }
+
+//     for(int i=1;i<=n;i++){
+//         if(cost[i]==inf){
+//             print("IMPOSSIBLE");
+//             return 0;
+//         }
+//     }
+//     cout<<ans;
+//     return 0;
+// }
