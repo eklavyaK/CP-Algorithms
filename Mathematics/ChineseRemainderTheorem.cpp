5abe8288471bc3387e349d5679efd1a5934e4426
eklavya <eklavyakumarsingh100@gmail.com>
2023-06-17 23:05:47 +0530

    commit

--- Mathematics/ChineseRemainderTheorem.cpp
+++ Mathematics/ChineseRemainderTheorem.cpp
@@ -64,14 +64,15 @@ triples extendedEuclidean(int a, int b){
     return ans;
 }
 int merge(int a, int n, int b, int m, int lc){
-    if(b<a) swap(a,b),swap(n,m);
     triples curr = extendedEuclidean(n,m);
     if((b-a) % curr.g != 0) return -1;
-    int r = (b-a)/curr.g;
-    int ans = (a + (curr.x*r % (m/curr.g))*n % lc) % lc;
-    return (ans + lc) % lc;
+    int r = ((b-a)/curr.g) % (m/curr.g);
+    curr.x = (curr.x % (m/curr.g) + (m/curr.g)) % (m/curr.g);
+    int u = r * curr.x % (m/curr.g);
+    int ans =  (a + u * n % lc) % lc;
+    return (ans % lc + lc) % lc;
 }
-int CRT(vector<int> a, vector<int> m){    // vectors of remainders (a) when x is divided by values (m)
+int CRT(vector<int> a, vector<int> m){    // remainders (a) when x is divided by values (m)
     int n = a.size();
     if(n==0) return -1;
     for(int i=0;i<n;i++){
@@ -87,14 +88,20 @@ int CRT(vector<int> a, vector<int> m){    // vectors of remainders (a) when x is
     }
     return r;
 }
+
+
 void code(int TC){
-    int a,n,b,m; cin>>a>>n>>b>>m;
-    vector<int> aa({a,b});
-    vector<int> md({n,m});
-    int ans = CRT(aa,md);
-    if(ans==-1) cout<<"no solution"<<endl;
-    else cout<<ans<<" "<<n*m/gcd(n,m)<<endl;
+    int n; cin>>n;
+    vector<int> a(n),m(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i]>>m[i];
+    }
+    int ans =  CRT(m,a);
+    cout<<"Case "<<TC<<": ";
+    if(ans==-1) cout<<"Impossible"<<endl;
+    else cout<<ans<<endl;
 }
+
 
 signed main(){
     cin.tie(0)->sync_with_stdio(0);
@@ -103,3 +110,30 @@ signed main(){
         code(TC);
     return 0;
 }
+/*
+2
+10
+13 2
+17 7
+37 35
+29 9
+31 13
+23 2
+11 7
+19 11
+5 2
+2 1
+12
+13 7
+29 5
+19 11
+11 7
+7 2
+31 8
+2 1
+5 1
+3 1
+17 1
+23 2
+37 16
+*/
